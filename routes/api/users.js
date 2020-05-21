a321c4d75e3ab3d7d1371cbfa95329c2e705455b
Stefan Lazovic <agios.stefanos@hotmail.com>
2020-05-21 16:14:04 +0200

    implementing jwt

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
@@ -42,7 +44,22 @@ router.post(
       await user.save();
 
       // return jsonwebtoken (if you wanna be logged in immediately when register an user, you have to return this token)
-      res.send('User registered');
+      const payload = {
+        user: {
+          id: user.id
+        }
+      }
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server error');
